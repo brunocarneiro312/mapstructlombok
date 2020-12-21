b24d39d8c74c98f3dd15fae401c2ef1c5e340b92
Bruno Carneiro <bruno.carneiro@netpos.com.br>
2020-12-21 13:10:39 -0300

    tests

--- src/test/java/com/sandbox/mapstructlombok/mapper/PersonMapperTest.java
+++ src/test/java/com/sandbox/mapstructlombok/mapper/PersonMapperTest.java
@@ -38,5 +38,21 @@ class PersonMapperTest {
     @Test
     @DisplayName("Dado uma person, quando mapear para personDTO, então retorna uma personDTO.")
     void toDTO() {
+
+        // given
+        Person person = new Person();
+        person.setId(UUID.randomUUID().toString());
+        person.setName("TestName");
+        person.setEmail("testname@email.com");
+
+        // when
+        PersonDTO personDTO = mapper.toDTO(person);
+
+        // then
+        assertAll("Testando parsing para Person...",
+                () -> assertNotNull(personDTO, "A pessoa não deveria estar nula"),
+                () -> assertNotNull(personDTO.getId(), "O ID não deveria estar nulo"),
+                () -> assertEquals(person.getName(), personDTO.getName(), "Os nomes deveriam ser iguais"),
+                () -> assertEquals(person.getEmail(), personDTO.getEmail(), "Os emails deveriam ser iguais"));
     }
 }
