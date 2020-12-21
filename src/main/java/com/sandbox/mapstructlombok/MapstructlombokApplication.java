5f0d31830c402b51ff5e7347c0a33b618b754b2d
Bruno Carneiro <bruno.carneiro@netpos.com.br>
2020-12-21 12:49:02 -0300

    setup

---
+++ src/main/java/com/sandbox/mapstructlombok/MapstructlombokApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.mapstructlombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructlombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructlombokApplication.class, args);
+    }
+
+}
